738300d1f52d8b66373b20b92fd6a3a38c1faa48
Anki <anki@Ankis-MacBook-Pro.local>
2020-05-13 15:00:46 -0700

    made request to owm

--- server/index.js
+++ server/index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+var requestPromise = require('request-promise');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/plz', (req, res) => {
     console.log('butt');
+    requestPromise(`http://api.openweathermap.org/data/2.5/weather?q=Seattle&appid=${process.env.owmApiKey}`)
+        .then((response) => {
+            console.log(response);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+
     res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify({ happycat: `buttdump` }));
 })
